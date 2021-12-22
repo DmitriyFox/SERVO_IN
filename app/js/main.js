5f293642b4d44526e924f0c4bd97d7010b8e95f9
DmitriyFox <gazpress_dima@mail.ru>
2021-12-22 13:37:17 +0500

    Подчистил слайдер, добавил фон в main, переименовал папку images.

--- app/js/main.js
+++ app/js/main.js
@@ -9,8 +9,8 @@ $(function () {
     });
 
     $('.slider__box').slick({
-        prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow-left">',
-        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow-right">',
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="img/arrow-left.svg" alt="arrow-left">',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="img/arrow-right.svg" alt="arrow-right">',
     });
 
 })
--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ function cleanDist() {
 }
 
 function images() {
-  return src('app/images/**/*')
+  return src('app/img/**/*')
     .pipe(imagemin(
       [
         imagemin.gifsicle({ interlaced: true }),
@@ -35,7 +35,7 @@ function images() {
         })
       ]
     ))
-    .pipe(dest('dist/images'))
+    .pipe(dest('dist/img'))
 }
 
 function scripts() {
